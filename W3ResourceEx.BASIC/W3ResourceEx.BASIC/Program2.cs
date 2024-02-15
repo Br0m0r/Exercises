a9e0efe6466eff0f54766d3bf731ec4dc0dd2a98
Br0m0r <158762685+Br0m0r@users.noreply.github.com>
2024-02-15 18:14:54 +0000

    added new file

---
+++ W3ResourceEx.BASIC/W3ResourceEx.BASIC/Program2.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+
+
+
+
+
+#region Exercise 1
+/*
+int[] array =new int[10];
+for (int i = 0;i<array.Length;i++)
+{
+    int a;
+    Console.WriteLine("enter a number");
+    int.TryParse(Console.ReadLine(),out a);
+    array[i] = a;
+}
+
+
+//Console.WriteLine($"Here is your array: {array}");//big mistake i made,should instead print array[i] on seperate loop
+Console.WriteLine("here are your numbers");
+for (int i = 0;i< 10;i++)
+{
+    Console.WriteLine(array[i]);
+}
+*/
+#endregion
+
+#region Exercise 
+
+
+#endregion
+
+#region Exercise 
+
+
+#endregion
+
+#region Exercise 
+
+
+#endregion
+
+#region Exercise 
+
+
+#endregion
