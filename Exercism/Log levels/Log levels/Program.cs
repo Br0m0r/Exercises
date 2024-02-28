8096e8129dadf3a96c31d9230a549bce3295dd08
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-02-28 19:49:07 +0200

    exercise

---
+++ Exercism/Log levels/Log levels/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace LogLevels
+{
+    class Logline
+    {
+        public static string Message(string logLine)
+        {
+            int endIndex = logLine.IndexOf("]:") + 2;
+            string message = logLine.Substring(endIndex).Trim();
+            return message;
+
+        }
+
+        public static string LogLevel(string logLine)
+        {
+           
+            int startIndex = logLine.IndexOf("[") + 1;
+            int endIndex = logLine.IndexOf("]");
+            string logLevel = logLine.Substring(startIndex, endIndex - startIndex).ToLower();
+            return logLevel;
+        }
+
+        public static string Reformat(string logLine)
+        {
+           
+            string message = Message(logLine);
+            string logLevel = LogLevel(logLine);
+            return $"{message} ({logLevel})";
+        }
+
+        static void Main()
+        {
+            string testLogLine1 = "[ERROR]: Invalid operation";
+            string testLogLine2 = "[WARNING]:  Disk almost full";
+            string testLogLine3 = "[INFO]: Operation completed";
+
+            Console.WriteLine(Message(testLogLine1));
+            Console.WriteLine(Message(testLogLine2)); 
+
+            Console.WriteLine(LogLevel(testLogLine1)); 
+            Console.WriteLine(LogLevel(testLogLine2)); 
+
+            Console.WriteLine(Reformat(testLogLine3)); 
+        }
+
+
+
+    } 
+
+
+
+}
