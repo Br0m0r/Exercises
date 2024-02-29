84e2c790491651a65236ae1e350c583839d4d147
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-02-29 20:40:04 +0200

    More exercises

---
+++ Exercism/Cars, Assemble!/Cars, Assemble!/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Exercise
+{
+    class AssemblyLine
+    {
+        
+        public static double SuccessRate(int speed)
+        {
+            
+            if (speed == 0)
+            {
+                return 0.0;
+            }
+            else if (speed >= 1 && speed <= 4)
+            {
+                return 1.0;
+            }
+            else if (speed >= 5 && speed <= 8)
+            {
+                return 0.9;
+            }
+            else if (speed == 9)
+            {
+                return 0.8;
+            }
+            else 
+            {
+                return 0.77;
+            }
+        }
+
+        public static double ProductionRatePerHour(int speed)
+        {
+            int CarsPerHour = 221 * speed;
+            return SuccessRate(speed) * CarsPerHour;  
+        }
+
+
+        public static int WorkingItemsPerMinute(int speed)
+        {
+            int CarsPerHour = 221 * speed;
+            int CarsPerMinute = CarsPerHour / 60;
+            double ToConvert = CarsPerMinute * SuccessRate(speed);
+            int ToReturn = (int)ToConvert;
+            return ToReturn;
+        }
+
+
+        static void Main()
+        {
+
+        }
+        
+
+    }
+}
---
+++ Exercism/Interest is Interesting/Interest is Interesting/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Exercise
+{
+    class SavingsAccount
+    {
+        static double InterestRate(double balance = 200.75)
+        {
+            if (balance < 0)
+            {
+                return 3.213;
+            }
+            else if (balance < 1000 && balance >= 0)
+            {
+                return 0.5;
+            }
+            else if (balance >= 1000 && balance < 5000)
+            {
+                return 1.621;
+            }
+            else
+            {
+                return 2.475;
+            }
+
+        }
+
+        static decimal Interest(double balance = 200.75)
+        {
+           double result = balance * InterestRate(balance) / 100;
+            return (decimal)result;
+        }
+
+        static decimal AnnualBalanceUpdate(double balance = 200.75)
+        {
+            return Interest(balance) + (decimal)balance;
+        }
+
+        static int YearsBeforeDesiredBalance(double balance = 200.75, double DesiredBalance = 250.75)
+        {
+            int counter = 0;
+            decimal balance2 = (decimal)balance;
+            decimal DesiredBalance2 = (decimal)DesiredBalance;
+            do
+            {
+
+                balance2 += Interest((double)balance2);
+                counter += 1;
+
+
+            }
+            while (balance2 <= DesiredBalance2);
+            return counter;
+        }
+
+       
+    }
+
+
+}
