f7b59e1ee486fc858e0589909885aebb16317d2a
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-01 15:05:17 +0200

    new

---
+++ Working with class and Objects-Pluralsight/Working with class and Objects-Pluralsight/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    internal class Book
+    {
+        private  List<double> grades;
+        private protected  string name;
+        
+
+        public Book(string name)
+        {
+
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics
+            {
+                Average = 0.0,
+                High = double.MinValue,
+                Low = double.MaxValue
+            };
+
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                result.Average += grade;
+            }
+
+            if (grades.Count > 0)
+            {
+                result.Average /= grades.Count;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Working with class and Objects-Pluralsight/Working with class and Objects-Pluralsight/Statistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    internal class Statistics
+    {
+        public double Average { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+
+        
+
+        
+
+    }
+
+
+}
