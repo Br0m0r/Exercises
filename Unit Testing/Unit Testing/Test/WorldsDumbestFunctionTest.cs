9696619139f3309a51949fac736eda0ef0dd591e
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-02 12:51:16 +0200

    New

---
+++ Unit Testing/Unit Testing/Test/WorldsDumbestFunctionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing.Test
+{
+    public class WorldsDumbestFunctionTest
+    {
+        //Naming Convention - ClassName_MethodName_ExceptionResult
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange-Get whatever u need(variables,classes,methods)and put it in here
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act Execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert - whatever is returned,is what we want
+                if (result == "PIKACHU")
+                {
+                    Console.WriteLine("PASSED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnsString");
+                }
+                else
+                {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnsString");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Unit Testing/Unit Testing/WorldsDumbestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing
+{
+    public class WorldsDumbestFunction
+    {
+        public string ReturnsPikachuIfZero(int numb)
+        {
+            if(numb == 0)
+            {
+                return "PIKACHU";
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+    }
+}
