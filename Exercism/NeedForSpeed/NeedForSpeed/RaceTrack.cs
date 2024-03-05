c23bba94db72c2e2a762f55260e6ce847e6bc99f
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-05 18:44:33 +0200

    exercise

---
+++ Exercism/NeedForSpeed/NeedForSpeed/RaceTrack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    internal class RaceTrack
+    {
+        private int distance {  get; set; }
+
+        public RaceTrack(int distance)
+        {
+            this.distance = distance;
+        }
+
+        public bool TryFinishTrack(RemoteControlCar car)
+        {
+            while (!car.BatteryDrained())
+            {
+                car.Drive();
+            }
+            return car.DistanceDriven() >= distance;
+        }
+    }
+}
---
+++ Exercism/NeedForSpeed/NeedForSpeed/RemoteControlCar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    internal class RemoteControlCar
+    {
+        internal int speed {  get; set; }
+        internal int batteryDrain { get; set; }
+        private int distanceDriven { get; set; }
+        private int battery = 100;
+
+        public RemoteControlCar(int speed,int batteryDrain)
+        {
+            this.speed = speed;
+            this.batteryDrain = batteryDrain;
+
+        }
+               
+        public void Drive()
+        {
+            if (battery >= batteryDrain)
+            {
+                distanceDriven += speed;
+                battery -= batteryDrain;
+            }
+        }
+         
+
+
+        public int DistanceDriven()
+        {
+            return distanceDriven;
+        }
+
+        public bool BatteryDrained()
+        {
+            if (battery <= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static RemoteControlCar Nitro()
+        {
+            return new RemoteControlCar(50, 4);
+        }
+
+
+
+
+    }
+}
