38deca29baee9de27d321ff299293253c2241aba
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-05 17:05:14 +0200

    new

---
+++ More_Exercises/SuggestedToDo/SuggestedToDo/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestedToDo
+{
+    internal class Book
+    {
+        private string title  { get; set; }
+        private string author { get; set; }
+        private int pages  { get; set; }
+        public Book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages; 
+        }
+
+        public void PrintNumPages()
+        {
+            Console.WriteLine($"The book currently has : {pages} pages");
+        }
+
+        public Book CutPage()
+        {
+            pages -= 1;
+            return this;
+        }
+
+    }
+}
---
+++ More_Exercises/SuggestedToDo/SuggestedToDo/Program.cs
@@ -0,0 +1,21 @@
+﻿using SuggestedToDo;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book = new Book("MyFartDiary","SuperiorBenignOverlord",100);
+           book.PrintNumPages();
+           Console.WriteLine("");
+           book.CutPage();
+           book.PrintNumPages();
+           Console.WriteLine();
+           book.CutPage().PrintNumPages();
+
+
+
+        }
+    }
+}
