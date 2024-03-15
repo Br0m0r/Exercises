bccc037e012055e4d8b1f799371eec56db0e4920
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-15 14:25:19 +0200

    Exercise added

---
+++ Exercism/D&D Character/D&D Character/CalcGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_D_Character
+{
+    public class CalcGen
+    {
+        private static Random random = new Random();
+
+        internal static int GenerateAbilityScore()
+        {
+            int[] rolls = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                rolls[i] = random.Next(1, 7);
+            }
+
+            Array.Sort(rolls);
+
+            return rolls[1] + rolls[2] + rolls[3];
+        }
+
+        internal static int CalculateHitpoints(int constitution)
+        {
+            int modifier = (constitution - 10) / 2;
+            return 10 + modifier;
+        }
+    }
+}
---
+++ Exercism/D&D Character/D&D Character/Program.cs
@@ -0,0 +1,26 @@
+﻿using D_D_Character;
+using System;
+
+public class DnDCharacterGenerator : CalcGen
+{
+    
+
+    public static void Main()
+    {
+        int[] abilities = new int[6];  
+        string[] abilityNames = { "Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma" };
+
+        
+        for (int i = 0; i < 6; i++)
+        {
+            abilities[i] = GenerateAbilityScore();
+            Console.WriteLine($"{abilityNames[i]}: {abilities[i]}");
+        }
+
+       
+        int hitpoints = CalculateHitpoints(abilities[2]);  
+        Console.WriteLine($"Hitpoints: {hitpoints}");
+    }
+
+    
+}
